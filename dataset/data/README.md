4c1eaf5478b5a9f3b823c4d32eb09f095e3e80f3
qiaw99 <872907968@qq.com>
2024-04-25 10:59:08 +0200

    Merge devset and testset

--- dataset/data/README.md
+++ dataset/data/README.md
@@ -4,5 +4,5 @@
 | count_data.txt | countdata(list)            | countdata           | -                     | Count instances |
 | show.txt       | show(list)                 | show                | -                     | Show a list of instances |
 | label.txt      | label(dataset)             | label               | -                     | Describe the label distribution|
-| keywords.txt   | keywords(topk, level)      | keywords topk level | topk, level           |Show most/less common words|
+| keywords.txt   | keywords(topk, level)      | keywords topk level | topk, level           |Show most common words|
 | similarity.txt | similarity(instance, topk) | similar topk        |topk                     | Show most similar instances| 
--- dataset/utils.py
+++ dataset/utils.py
@@ -7,7 +7,7 @@ def output_number_of_pairs_per_file():
     Merge all (user question, parsed text) pairs, calculate the total number of pairs and store them in jsonl file
     """
     prefix = "./"
-    folder_names = [i for i in os.listdir() if os.path.isdir(prefix + i) and i != "talktomodel" and i != "work_in_progress"]
+    folder_names = [i for i in os.listdir() if os.path.isdir(prefix + i) and i != "filter" and i != "talktomodel" and i != "work_in_progress"]
 
     output = []
     user_question = []
@@ -17,20 +17,21 @@ def output_number_of_pairs_per_file():
         files = os.listdir(prefix + folder_name)
 
         for f in files:
-            before = len(user_question)
+            if f.endswith(".txt"):
+                before = len(user_question)
 
-            f_pointer = open(f"./{folder_name}/{f}", "r")
-            lines = [line.strip() for line in f_pointer.readlines()]
+                f_pointer = open(f"./{folder_name}/{f}", "r")
+                lines = [line.strip() for line in f_pointer.readlines()]
 
-            for l in lines:
-                if l.startswith("User"):
-                    user_question.append(l)
-                elif l.startswith("Parsed"):
-                    parsed_text.append(l)
+                for l in lines:
+                    if l.startswith("User"):
+                        user_question.append(l.split("User: ")[1])
+                    elif l.startswith("Parsed"):
+                        parsed_text.append(l.split("Parsed: ")[1])
 
-            after = len(user_question)
+                after = len(user_question)
 
-            print(f"{f}: {after-before}")
+                print(f"{f}: {after-before}")
 
     print(f"In total: {len(user_question)}")
 
