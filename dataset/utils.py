3b91e67b42a7b2a4dbf7bbd60fd4f851affec0c6
qiaw99 <872907968@qq.com>
2024-04-27 12:19:03 +0200

    upload train/test sets

--- dataset/utils.py
+++ dataset/utils.py
@@ -1,6 +1,23 @@
 import json
 import os
 
+from sklearn.model_selection import train_test_split
+
+
+def save_dataset_as_json(X, y, split="train"):
+    output = []
+    for i in range(len(X)):
+        output.append({
+            "idx": i,
+            "text": X[i],
+            "sql": y[i]
+        })
+
+    jsonString = json.dumps(output)
+    jsonFile = open(f"coxql_{split}.json", "w")
+    jsonFile.write(jsonString)
+    jsonFile.close()
+
 
 def output_number_of_pairs_per_file():
     """
@@ -9,7 +26,6 @@ def output_number_of_pairs_per_file():
     prefix = "./"
     folder_names = [i for i in os.listdir() if os.path.isdir(prefix + i) and i != "filter" and i != "talktomodel" and i != "work_in_progress"]
 
-    output = []
     user_question = []
     parsed_text = []
 
@@ -35,16 +51,10 @@ def output_number_of_pairs_per_file():
 
     print(f"In total: {len(user_question)}")
 
-    for i in range(len(user_question)):
-        output.append({
-            "idx": i,
-            "text": user_question[i],
-            "sql": parsed_text[i]
-        })
+    X_train, X_test, y_train, y_test = train_test_split(user_question, parsed_text, shuffle=True, random_state=42, test_size=0.2)
 
-    jsonString = json.dumps(output)
-    jsonFile = open(f"final_coxql.json", "w")
-    jsonFile.write(jsonString)
-    jsonFile.close()
+    save_dataset_as_json(X_train, y_train, "train")
+    save_dataset_as_json(X_test, y_test, "test")
+
 
 output_number_of_pairs_per_file()
